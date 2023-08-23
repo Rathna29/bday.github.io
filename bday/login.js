3174eff9f1da299bbc7af0590da04ab8393e50cf
Rathna29 <142040101+Rathna29@users.noreply.github.com>
2023-08-24 00:12:11 +0530

    added

---
+++ bday/login.js
@@ -0,0 +1,44 @@
+var username=document.forms['form']['username'];
+var password=document.forms['form']['Password'];
+
+var user_error=document.getElementById('user_error');
+var pass_error=document.getElementById('pass_error');
+
+username.addEventListener('textInput',username_verify);
+password.addEventListener('textInput',password_verify);
+
+function validated()
+{
+    if(username.value.length<10)
+    {
+        username.style.border="1px solid red";
+        user_error.style.display="block";
+        username.focus();
+        return false;
+    }
+    if(password.value.length<10)
+    {
+        password.style.border="1px solid red";
+        pass_error.style.display="block";
+        password.focus();
+        return false;
+    }
+    function username_verify()
+    {
+        if(username.value.length>=10)
+        {
+            username.style.border="1px solid silver";
+            user_error.style.display="none";
+            return true;
+        }
+        function password_verify()
+        {
+            if(password.value.length>=5)
+            {
+                password.style.border="1px solid silver";
+                password_error.style.display="none";
+                return true;
+            }
+    }
+}
+}
